362e93fa7b71a5c3132f68d777aeb1cdfa353c65
Anianias7 <anna.skrzeszewska95@gmail.com>
2018-07-09 13:45:49 +0200

    Version without avereage for a year

--- cos.js
+++ cos.js
@@ -5,13 +5,38 @@ var marksInfoHeader = [...document.querySelectorAll('.semester_headers')];
 
 var marksInfoTable = [...document.querySelectorAll('.semester')];
 
+console.log(marksInfoTable);
+
+//Lista ["Semestr akademicki Letni(2017/2018), Rok studiów 3, Semestr studiów 6"]
 var listOfSemestersInfo = marksInfoHeader.map(header => header.firstElementChild.innerText);
 
+//Lista lat [3,3,2,2,1]
 var listOfYearsNumbers = listOfSemestersInfo.map(header => header.split(',')[1].split(" ")[3]);
 
+//Lista semestrów [6,5,4,3,2]
+var listOfSemestersNumbers = listOfSemestersInfo.map(header => header.split(',')[2].split(" ")[3]);
+console.log(listOfSemestersNumbers);
+
+
+var uniq = (arrArg) => {
+    return arrArg.filter((elem, pos, arr) => {
+      return arr.indexOf(elem) == pos;
+    });
+  }
+
+
+//Lista ectsów dla ocen z semestrów 
 var listOfEctsForSemesters = marksInfoTable.map(table => 
-                                    [...table.children].map(el => el.lastElementChild.innerText
+                                    [...table.children].map(el => {
+                                        if(isNaN(el.lastElementChild.innerText)){
+                                            if(el.lastElementChild.innerText === "k.cz.1")
+                                                return 0;      
+                                            return el.lastElementChild.firstChild.firstChild.lastChild.innerText;
+                                        } 
+                                        return el.lastElementChild.innerText
+                                    }
                                 ));
+
 
 var listOfMarksForSemesters = marksInfoTable.map(table => 
                                     [...table.children].map(el => {
@@ -54,5 +79,5 @@ var averageSemestersInfo = zip(listOfSemestersInfo,
                                (info, average) => [info, average]);
 
 
-console.log(averageSemestersInfo[0][1]);
+
 averageSemestersInfo;
--- test.js
+++ test.js
@@ -8,13 +8,13 @@ window.addEventListener('DOMContentLoaded', function(){
             var row = document.createElement('tr');
             var text = document.createElement('td');
             var average = document.createElement('td');
-            var yearAverage = document.createElement('td');
+            // var yearAverage = document.createElement('td');
             text.appendChild(document.createTextNode(averageArrayInfo[i][0]));
             average.appendChild(document.createTextNode(averageArrayInfo[i][1]));
-            yearAverage.appendChild(document.createTextNode("Buuu"));
+            // yearAverage.appendChild(document.createTextNode("Buuu"));
             row.appendChild(text);
             row.appendChild(average);
-            row.appendChild(yearAverage);
+            // row.appendChild(yearAverage);
             row.classList.add('table-row');
             text.classList.add('text');
             average.classList.add('average');
